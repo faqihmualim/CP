cecd51d20643d1f7d0f16d0e41cfde535ca77432
Faqih Mualim <65641421+faqihmualim@users.noreply.github.com>
2020-05-21 15:12:31 +0700

    Create Template Codeforces.cpp

---
+++ Template Codeforces.cpp
@@ -0,0 +1,29 @@
+// Template for Codeforces
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forian(i,a,n) for(ll i=a;i<=n;i++)
+#define pb push_back
+#define popb pop_back
+#define mp make_pair
+#define ll long long
+
+int N; int a=0;
+int arr[110];
+
+void test_case()
+{
+	// Type Your Body of Code Here
+}
+
+int main() // Main Program for Codeforces
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--);
+	test_case;
+
+return 0;
+}
