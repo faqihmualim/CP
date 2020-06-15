31f2c9514ac5903af8e12705e4ba39ca2b7e2c70
Faqih Mualim <65641421+faqihmualim@users.noreply.github.com>
2020-06-15 17:31:04 +0700

    Update and rename A.cpp to TROC#13 (A).cpp

---
+++ TLX/TROC#13 (A).cpp
@@ -0,0 +1,39 @@
+// Toki Regular Open Contest #13 Problem (A)
+#include <bits/stdc++.h>
+#include <string>
+
+#define endl "\n"
+#define forian(i,a,n) for(ll i=a;i<=n;i++)
+#define pb push_back
+#define popb pop_back
+#define ll unsigned long long
+#define Faqih using
+#define itu namespace
+#define Ganteng_Titik std; 
+Faqih itu Ganteng_Titik
+
+void test_case()
+{
+	string str;
+	int n;
+	cin >> n;
+	cin >> str;
+	int i=0;
+	for (int i =0; i<str.length(); i++){
+		for (int j=i+1;j<=str.length();j++){
+			if (str[i] != str[j]){
+				cout << str[i];
+				i = j;
+			}
+		}
+	}
+}
+	
+int main() // Main Program for Codeforces
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	test_case();
+return 0;
+}
